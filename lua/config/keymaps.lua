74d36fef80dd17518d1896fdc04bf2be8fd97dc2
vasiliigogolev <vasiliigogolev92@gmail.com>
2024-03-07 16:49:22 +0700

    replace nvim_tree with neotree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,13 +7,12 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', 'dd', '"_dd', opts)
 vim.keymap.set('n', 'd', '"_d', opts)
 vim.keymap.set('v', 'd', '"_d', opts)
-
+vim.api.nvim_set_keymap('n', '<C-b>', '<Cmd>Neotree toggle<CR>', opts)
 vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 vim.keymap.set('n', '<C-k>', ':m-2<CR>', opts)
 vim.keymap.set('n', '<C-j>', ':m+1<CR>', opts)
 
-vim.api.nvim_set_keymap('n', '<C-b>', '<Cmd>NvimTreeToggle<CR>', opts)
 
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
