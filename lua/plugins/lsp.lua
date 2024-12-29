10488f3e6aa8a4e9b1ba58ca2b63cfe888025cb1
vgglv <ripykt@gmail.com>
2024-12-29 18:46:58 +0900

    Switch lsp next-prev to C-p, C-n

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -116,13 +116,9 @@ return {
 					end,
 				},
 				mapping = {
-					["<Tab>"] = cmp.mapping(function(fallback)
+					["<C-n>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-							-- that way you will only jump inside the snippet region
---						elseif luasnip.expand_or_jumpable() then
---							luasnip.expand_or_jump()
 						elseif has_words_before() then
 							cmp.complete()
 						else
@@ -130,7 +126,7 @@ return {
 						end
 					end, { "i", "s" }),
 
-					["<S-Tab>"] = cmp.mapping(function(fallback)
+					["<C-p>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
 						elseif luasnip.jumpable(-1) then
