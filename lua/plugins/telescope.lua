3c48bc6b0b462f06a2e6611b04007c70a0aba7c7
vgglv <ripykt@gmail.com>
2025-01-22 11:58:30 +0900

    update cfg

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,15 +7,42 @@ return {
 	},
 	config = function()
 		local telescope = require("telescope")
-		--local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = require("telescope.themes").get_ivy({
+				layout_strategy = "bottom_pane",
+				layout_config = {
+					height = 30,
+				},
 
-		telescope.setup({})
-
+				border = true,
+				borderchars = {
+					prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
+					results = { " " },
+					preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				},
+			}),
+			extensions = {
+				fzf = {}
+			}
+		})
 		telescope.load_extension("fzf")
+
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<Leader>sf', builtin.find_files, {noremap=true, desc = 'Search files'})
-		vim.keymap.set('n', '<Leader>sg', builtin.live_grep, {noremap=true, desc = 'Live grep'})
-		vim.keymap.set('n', '<Leader><Leader>', builtin.buffers, {noremap=true, desc = 'Buffers'})
-		vim.keymap.set('n', '<Leader>sr', builtin.resume, {noremap=true, desc='Resume last picker'})
+		vim.keymap.set("n", "<Leader>sf", builtin.find_files)
+		vim.keymap.set("n", "<Leader>sh", builtin.help_tags)
+		vim.keymap.set("n", "<Leader>sr", builtin.resume)
+		vim.keymap.set("n", "<Leader><Leader>", builtin.buffers)
+		vim.keymap.set("n", "<Leader>en", function()
+			builtin.find_files({
+				cwd = vim.fn.stdpath("config")
+			})
+		end)
+		vim.keymap.set("n", "<Leader>ep", function()
+			builtin.find_files({
+				cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+			})
+		end)
+
+		require("config.telescope.multigrep").setup()
 	end,
 }
