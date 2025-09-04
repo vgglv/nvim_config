4d13ff0d0f3d7dad3fc9bc622dcabbfe1c351add
vgglv <ripykt@gmail.com>
2025-09-04 13:35:47 +0900

    added vim.lsp.buf.incoming_calls keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,22 +21,25 @@ vim.keymap.set("v", "<C-n>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-p>", ":m '<-2<CR>gv=gv")
 
 opts.desc = "Show LSP references"
-vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
 opts.desc = "Go to declaration"
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
 opts.desc = "Show LSP definitions"
-vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
 opts.desc = "Show LSP implementations"
-vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
 opts.desc = "Show LSP type definitions"
-vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
 opts.desc = "See available code actions"
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+opts.desc = "Incoming calls hierarchy"
+vim.keymap.set({ "n", "v" }, "<leader>ci", vim.lsp.buf.incoming_calls, opts)
 
 opts.desc = "Smart rename"
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
