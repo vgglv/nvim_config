7d488a9fbf66cfa7973dc6af9f6ff9716bf1ac7f
vgglv <ripykt@gmail.com>
2025-09-04 09:54:49 +0900

    added search inside folder function in multigrep.lua

--- lua/config/telescope/multigrep.lua
+++ lua/config/telescope/multigrep.lua
@@ -45,8 +45,54 @@ local live_multigrep = function(opts)
 	}):find()
 end
 
+local ripgrep_in_folder = function(opts)
+	opts = opts or {}
+	opts.cwd = opts.cwd or vim.uv.cwd()
+
+	local finder = finders.new_async_job({
+		command_generator = function(prompt)
+			if not prompt or prompt == "" then
+				return nil
+			end
+
+			local pieces = vim.split(prompt, "  ", { trimempty = true })
+			local args = {"rg"}
+			if pieces[1] then
+				table.insert(args, "-e")
+				table.insert(args, pieces[1])
+			end
+
+			vim.list_extend(args, {
+				"--color=never", 
+				"--no-heading", 
+				"--with-filename", 
+				"--line-number", 
+				"--column", 
+				"--smart-case" 
+			})
+
+			for i = 2, #pieces do
+				table.insert(args, pieces[i])
+			end
+
+			return args
+		end,
+		entry_maker = make_entry.gen_from_vimgrep(opts),
+		cwd = opts.cwd
+	})
+
+	pickers.new(opts, {
+		debounce = 100,
+		prompt_title = "Ripgrep in folder",
+		finder = finder,
+		previewer = conf.grep_previewer(opts),
+		sorter = require("telescope.sorters").empty(),
+	}):find()
+end
+
 M.setup = function()
 	vim.keymap.set("n", "<Leader>sg", live_multigrep)
+	vim.keymap.set("n", "<Leader>rg", ripgrep_in_folder)
 end
 
 return M
