a5a0e7764448447c55b0abfed9a0341164dff373
vgglv <ripykt@gmail.com>
2025-09-11 18:23:56 +0900

    switch theme to light theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,24 @@
+--return {
+--    "catppuccin/nvim",
+--    name = "catppuccin",
+--    priority = 1000,
+--    config = function(opts)
+--        vim.cmd.colorscheme("catppuccin-latte")
+--    end
+--}
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function(opts)
-        vim.cmd.colorscheme("catppuccin-mocha")
-    end
+	"Mofiqul/vscode.nvim",
+	config = function()
+		require("vscode").setup({
+			style = "light",
+			transparent = false,
+			italic_comments = true,
+			
+			color_overrides = {
+				vscLineNumber = "#CCCCCC"
+			},
+		})
+		require("vscode").load()
+	end,
+	priority = 1000,
 }
