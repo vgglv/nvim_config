266db711bc7ad1d063c3cc1a9c7b876e19bd5c71
vgglv <ripykt@gmail.com>
2025-09-12 10:47:53 +0900

    change theme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,24 +1,7 @@
---return {
---    "catppuccin/nvim",
---    name = "catppuccin",
---    priority = 1000,
---    config = function(opts)
---        vim.cmd.colorscheme("catppuccin-latte")
---    end
---}
 return {
-	"Mofiqul/vscode.nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	config = function()
-		require("vscode").setup({
-			style = "light",
-			transparent = false,
-			italic_comments = true,
-			
-			color_overrides = {
-				vscLineNumber = "#CCCCCC"
-			},
-		})
-		require("vscode").load()
-	end,
-	priority = 1000,
+		vim.cmd("colorscheme rose-pine")
+	end
 }
