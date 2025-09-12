3feaa490ed5ea69f60be163ca06bbf02ac607819
vgglv <ripykt@gmail.com>
2025-09-12 10:48:57 +0900

    rm indent-blankline, vim-illuminate

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,92 +1,37 @@
 return {
-    { -- this stuff changes select and input to a floating windows
-        "stevearc/dressing.nvim",
-        lazy = true,
-        enabled = true,
-        init = function()
-            vim.ui.select = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.select(...)
-            end
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.input = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.input(...)
-            end
-        end
-    },
-    { -- this stuff highlights indentation lines
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            indent = {
-                char = "│",
-                tab_char = "│",
-            },
-            scope = { enabled = false },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-            },
-        },
-        main = "ibl",
-    },
-    { -- this shows a status bar at the bottom of editor
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require('lualine').setup({
-                sections = {
-                    lualine_c = { { 'filename', path = 1 } }
-                }
-            })
-        end,
-    },
-    { -- this highlights all the similar texts inside an editor
-        "RRethy/vim-illuminate",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            delay = 200,
-            large_file_cutoff = 2000,
-            large_file_overrides = {
-                providers = { "lsp" },
-            },
-        },
-        config = function(_, opts)
-            require("illuminate").configure(opts)
-
-            local function map(key, dir, buffer)
-                vim.keymap.set("n", key, function()
-                    require("illuminate")["goto_" .. dir .. "_reference"](false)
-                end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-            end
-
-            map("]]", "next")
-            map("[[", "prev")
-
-            -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-            vim.api.nvim_create_autocmd("FileType", {
-                callback = function()
-                    local buffer = vim.api.nvim_get_current_buf()
-                    map("]]", "next", buffer)
-                    map("[[", "prev", buffer)
-                end,
-            })
-        end,
-        keys = {
-            { "]]", desc = "Next Reference" },
-            { "[[", desc = "Prev Reference" },
-        },
-    }
+	{ -- this stuff changes select and input to a floating windows
+		"stevearc/dressing.nvim",
+		lazy = true,
+		enabled = true,
+		init = function()
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end
+	},
+	{ -- this shows a status bar at the bottom of editor
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require('lualine').setup({
+				options = {
+					icons_enabled = true,
+				},
+				sections = {
+					lualine_a = {'mode'},
+					lualine_b = {'diagnostics'},
+					lualine_c = { { 'filename', path = 1 } },
+					lualine_x = {'filetype'},
+					lualine_y = {},
+					lualine_z = {'location'}
+				}
+			})
+		end,
+	},
 }
