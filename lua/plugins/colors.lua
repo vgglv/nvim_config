2ed93d6365da88d9a554791aced4e5030b2cc882
vgglv <ripykt@gmail.com>
2025-09-12 16:02:43 +0900

    leave vscode theme for light mode

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,23 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require('rose-pine').setup({
+				disable_background = true,
+			})
+			--vim.cmd("colorscheme rose-pine")
+		end
+	},
+	{
+		"Mofiqul/vscode.nvim",
+		name = "vscode",
+		config = function()
+			require('vscode').setup({
+				transparent = true,
+				italic_comments = false,
+			})
+			vim.cmd("colorscheme vscode")
+		end
+	}
 }
