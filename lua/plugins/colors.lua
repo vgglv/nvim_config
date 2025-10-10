b85b63142b2054559031e123a58c4061c88bb36a
vgglv <ripykt@gmail.com>
2025-10-10 19:52:27 +0900

    gruvbox baby

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,8 +5,13 @@ return {
 		config = function()
 			require('rose-pine').setup({
 				disable_background = true,
+				styles = {
+					italic = false,
+					bold = true,
+					transparency = true
+				}
 			})
-			--vim.cmd("colorscheme rose-pine")
+			--vim.cmd.colorscheme "rose-pine"
 		end
 	},
 	{
@@ -20,7 +25,19 @@ return {
 					NeoTreeDimText = { bg = "NONE" },
 				},
 			})
-			vim.cmd("colorscheme vscode")
+			--vim.cmd.colorscheme "vscode"
+		end
+	},
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.g.gruvbox_material_foreground = 'material' -- [material,mix,original]
+			vim.g.gruvbox_material_enable_italic = false
+			vim.g.gruvbox_material_enable_bold = true
+			vim.g.gruvbox_material_background = 'hard' -- [soft, hard, medium]
+			vim.g.gruvbox_material_transparent_background = 0 -- 0,1,2
+
+			vim.cmd.colorscheme "gruvbox-material"
 		end
 	}
 }
