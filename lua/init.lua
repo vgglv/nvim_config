0130659d0f98b8d1bfb031a5fde3935d06050e89
vgglv <ripykt@gmail.com>
2026-09-14 11:39:00 +0900

    gruvbox

--- lua/init.lua
+++ lua/init.lua
@@ -17,7 +17,7 @@ local plugins_list = {
 	"https://github.com/lewis6991/gitsigns.nvim",
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 	"https://github.com/nvim-treesitter/nvim-treesitter-context",
-	"https://github.com/catppuccin/nvim.git"
+	"https://github.com/ellisonleao/gruvbox.nvim"
 }
 vim.pack.add(plugins_list)
 
